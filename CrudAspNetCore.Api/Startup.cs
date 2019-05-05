d4f8b75d6233a58b5c9dbcc2ab767631dc66898c
Sky Chang <skchang@microsoft.com>
2019-05-05 10:28:47 +0800

    Change Swagger Title

--- CrudAspNetCore.Api/Startup.cs
+++ CrudAspNetCore.Api/Startup.cs
@@ -45,7 +45,9 @@ namespace CrudAspNetCore.Api
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Employee API",
+                    Version = "v1", });
             });
         }
 
