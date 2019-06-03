94736d7404139778c58956c94898f99bbac1f742
Sky Chang <skchang@microsoft.com>
2019-06-03 15:46:54 +0800

    Test auto cd

--- CrudAspNetCore.Api/Controllers/ValuesController.cs
+++ CrudAspNetCore.Api/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CrudAspNetCore.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "AKS" };
         }
 
         // GET api/values/5
